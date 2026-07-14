fcbd11b3c009cff4627f276a986b73a2a80a0fa4
skyedulla <sohanyedulla@gmail.com>
2026-07-14 13:57:34 +0800

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.py
@@ -0,0 +1,43 @@
+class TimeMap:
+
+    def __init__(self):
+        self.box_of_keys = {}
+        
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key in self.box_of_keys:
+            inserted = False
+            for i in range(len(self.box_of_keys[key])):
+                if timestamp < self.box_of_keys[key][i][0]:
+                    self.box_of_keys[key].insert(i, (timestamp, value))
+                    inserted = True
+                if inserted == False:
+                    self.box_of_keys[key].append((timestamp, value))
+
+        else:
+            self.box_of_keys[key] = [(timestamp, value)]
+        
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key in self.box_of_keys:
+            #if the timestamp is less than the first key then no key will be valid
+            if timestamp >= self.box_of_keys[key][0][0]: 
+                low = 0
+                high = len(self.box_of_keys[key]) - 1
+
+                while low < high:
+                    mid = (low + high + 1) // 2
+                    if self.box_of_keys[key][mid][0] <= timestamp:
+                        low = mid
+                    else:
+                        high = mid - 1
+                
+                return self.box_of_keys[key][low][1]
+
+            else:
+                return ""
+        
+        else:
+            return ""
+        """this function will perform binary search on a set of numbers attempting to 
+        find the largest value that satisfies a condition"""
